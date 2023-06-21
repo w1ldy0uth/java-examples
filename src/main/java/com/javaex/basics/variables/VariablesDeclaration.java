d19ff07aad3537683ea81c7c1a2068746a927aac
Ivan Shurygin <shurygin1vs@gmail.com>
2023-06-21 12:51:41 +0300

    0.001 First method of Variables

---
+++ src/main/java/com/javaex/basics/variables/VariablesDeclaration.java
@@ -0,0 +1,11 @@
+package com.javaex.basics.variables;
+
+public class VariablesDeclaration {
+    public void variables() {
+        // Learn all variable types in Java
+
+        // Byte variables are 1-byte integers in range from -128 to 127
+        byte byteVar = -56;
+        System.out.println("Byte: " + byteVar);
+    }
+}
